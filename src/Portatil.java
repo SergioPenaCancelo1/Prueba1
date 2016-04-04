9e8752b35c82f35fbf2e1ce997aa70c289fc5fbb
dam124 <dam124@PC2021.aulas.local>
2016-04-04 12:59:36 +0200

    3 commit

---
+++ src/Portatil.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dam124
+ */
+public class Portatil extends Ordenador {
+    String pulgadas;
+
+    public String getPulgadas() {
+        return pulgadas;
+    }
+
+    public void setPulgadas(String pulgadas) {
+        this.pulgadas = pulgadas;
+    }
+}
